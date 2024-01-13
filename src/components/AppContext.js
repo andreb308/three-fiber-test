9c19350b051d2865cf7a6c55e81bd31efab88dee
andreb308 <107200960+andreb308@users.noreply.github.com>
2024-01-13 20:46:31 -0300

    feat: URL state initialization

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -3,8 +3,11 @@ import React, { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [title, setTitle] = useState("@andreb308");
-  const [textVisibility, setTextVisibility] = useState(true);
+  const queryParameters = new URLSearchParams(window.location.search);
+  const searchParam = queryParameters.get("title");
+  const visibilityParam = parseInt(queryParameters.get("visible"));
+  const [title, setTitle] = useState(searchParam || "@andreb308");
+  const [textVisibility, setTextVisibility] = useState(visibilityParam ? true : false);
 
   return (
     <AppContext.Provider
